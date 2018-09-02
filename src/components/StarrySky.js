10ed30df260f64f749bc9ef471acdc29bc2fd6c1
0Calories <cppanand@gmail.com>
2018-09-02 16:37:54 -0400

    Add randomly generated stars to StarrySky component (needs improvement)

--- src/components/StarrySky.js
+++ src/components/StarrySky.js
@@ -2,12 +2,65 @@ import React from 'react';
 
 import GreetingSign from './GreetingSign';
 
+const STAR_RADIUS = 3;
+
 export default class StarrySky extends React.Component {
+
+    state = {
+        stars: [],
+        numStars: 40
+    };
+
+    generateStar = () => {
+        console.log('generateStar() called');
+        return {
+            xPos: Math.random() * this.refs.canvas.width,
+            yPos: Math.random() * this.refs.canvas.height
+            // colour: Random colour from an array of selections
+        };
+    };
+
+    initializeStars = () => {
+        let starArray = [];
+        for (let i = 0; i < this.state.numStars; i++)
+            starArray.push(this.generateStar());
+
+        this.setState({ stars: starArray }, () => {
+            console.log(this.state.stars)
+
+            const canvas = this.refs.canvas;
+            canvas.width = canvas.clientWidth;
+            canvas.height = canvas.clientHeight;
+            const ctx = canvas.getContext("2d");
+
+
+            this.drawStars(canvas, ctx);
+        });
+    };
+
+    drawStars = (canvas, ctx) => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < this.state.numStars; i++) {
+            const star = this.state.stars[i];
+            ctx.beginPath();
+            ctx.arc(star.xPos, star.yPos, STAR_RADIUS, 0, Math.PI * 2, false);
+            ctx.fillStyle = "rgb(" + 
+                Math.floor(Math.random() * 256) + "," +
+                Math.floor(Math.random() * 256) + "," +
+                Math.floor(Math.random() * 256) + ")";
+            ctx.fill();
+        }
+    };
+
+    componentDidMount() {
+        this.initializeStars();
+    }
 
     render() {
         return (
             <div className="starry-sky">
-                <canvas ref="canvas" className="starry-sky__canvas" />
+                <canvas ref="canvas" width="640" height="260" className="starry-sky__canvas" />
                 <GreetingSign />
             </div>
         );
