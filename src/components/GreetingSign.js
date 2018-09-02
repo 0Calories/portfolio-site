60cb7ae2ecc69f91d3e6b35916f97e830525da46
0Calories <cppanand@gmail.com>
2018-09-02 12:42:08 -0400

    Add GreetingSign component with basic styling

---
+++ src/components/GreetingSign.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GreetingSign = () => (
+    <div className="greeting-sign">
+        <h1 className="greeting-sign__title">Hey, I'm Ash</h1>
+        <h3 className="greeting-sign__subtitle">Welcome to my page!</h3>
+    </div>
+);
+
+export default GreetingSign;
--- src/components/StarrySky.js
+++ src/components/StarrySky.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GreetingSign from './GreetingSign';
+
 export default class StarrySky extends React.Component {
 
     render() {
         return (
             <div className="starry-sky">
+                <GreetingSign />
             </div>
         );
     }
