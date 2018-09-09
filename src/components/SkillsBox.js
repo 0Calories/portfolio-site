46b78c07b554ba431cff39a29db9342d03d6364d
0Calories <cppanand@gmail.com>
2018-09-09 13:47:01 -0400

    Add more SkillBadge icons

--- src/components/SkillsBox.js
+++ src/components/SkillsBox.js
@@ -6,12 +6,18 @@ const SkillsBox = () => (
     <div className="skills-box box">
         <h1 className="skills-box__header box-header">My Skills</h1>
         <div className="skills-box__container">
+            <SkillBadge badgeName="Java" />
+            <SkillBadge badgeName="C++" />
             <SkillBadge badgeName="JavaScript" />
             <SkillBadge badgeName="HTML5" />
             <SkillBadge badgeName="CSS3" />
             <SkillBadge badgeName="NodeJS" />
+            <SkillBadge badgeName="React" />
+            <SkillBadge badgeName="Webpack" />
             <SkillBadge badgeName="MongoDB" />
-            <SkillBadge badgeName="Java" />
+            <SkillBadge badgeName="Mocha" />
+            <SkillBadge badgeName="Bootstrap" />
+            <SkillBadge badgeName="jQuery" />
         </div>
     </div>
 );
