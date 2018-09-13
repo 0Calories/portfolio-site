e3e28c2a38eb6924b1f07f41bc957282e9dc459e
0Calories <cppanand@gmail.com>
2018-09-13 16:11:31 -0400

    Update outdated image path for SkillBadge

--- src/components/SkillBadge.js
+++ src/components/SkillBadge.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SkillBadge = (props) => (
     <div>
         <img 
-            src={"/images/" + props.badgeName + ".png"}
+            src={"/images/skills/" + props.badgeName + ".png"}
             className="skill-badge"
         />
     </div>
