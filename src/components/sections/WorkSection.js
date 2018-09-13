51cc61a7fe44495dbccb516a6fa8ae79cb5219ef
0Calories <cppanand@gmail.com>
2018-09-13 19:29:11 -0400

    Add new description prop to WorkBox and update styling

--- src/components/sections/WorkSection.js
+++ src/components/sections/WorkSection.js
@@ -6,18 +6,22 @@ import WorkBox from '../WorkBox';
 const jobs = [{
     company: 'Cheetah Networks',
     date: 'March 2018 - Present',
+    description: 'Placeholder',
     badges: []
 }, {
     company: 'CNW Canada Newswire',
     date: 'September 2017 - December 2017',
+    description: 'Placeholder',
     badges: []
 }, {
     company: 'Ross Video',
     date: 'January 2017 - April 2017',
+    description: 'Placeholder',
     badges: []
 }, {
     company: 'Agriculture and Agri-Food Canada',
     date: 'June 2016 - December 2017',
+    description: 'Placeholder',
     badges: [] 
 }];
 
@@ -25,7 +29,7 @@ const WorkSection = (props) => (
     <div className="section work-section">
         <h1 className="section__header">{props.sectionName}</h1>
         <div className="section__body">
-            {jobs.map((job) => <WorkBox companyName={job.company} dateRange={job.date} badges={job.badges}/>)}
+            {jobs.map((job) => <WorkBox companyName={job.company} dateRange={job.date} description={job.description} badges={job.badges}/>)}
         </div>
     </div>
 );
