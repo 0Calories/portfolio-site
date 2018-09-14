4d6e02a0df44cccb54fa3541ec30c1245a1ce0e7
0Calories <cppanand@gmail.com>
2018-09-13 23:23:27 -0400

    Create spinning animation when project-icon clicked on

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -2,30 +2,49 @@ import React from 'react';
 
 import SkillBadge from './SkillBadge';
 
-const ProjectBox = (props) => (
-    <div className="project-box box">
-        <div className="project-box__header-container">
-            <h1 className="project-box__header box-header">{props.projectName}</h1>
-            <div className="project-box__header-strike">
-                <img
-                    src={"/images/projects/" + props.projectName + "-icon.png"}
-                    className="project-box__image"
-                />
+export default class ProjectBox extends React.Component {
+
+    state = {
+        opened: false,
+    };
+
+    toggleOpen = () => {
+        console.log(`On Click called! Opened=${!this.state.opened}`);
+
+        this.setState((prevState) => ({ opened: !prevState.opened }));
+    };
+
+    componentDidMount() {
+        this.refs.spinner
+    }
+
+    render() {
+        return (
+            <div className="project-box box">
+                <div className="project-box__header-container">
+                    <h1 className="project-box__header box-header">{this.props.projectName}</h1>
+                    <div className="project-box__header-strike">
+                        <img
+                            src={"/images/projects/" + this.props.projectName + "-icon.png"}
+                            ref='spinner'
+                            className={this.state.opened ? "project-box__image project-box__image--opened" : "project-box__image project-box__image--closed"}
+                            onClick={this.toggleOpen}
+                        />
+                    </div>
+                </div>
+
+                <div className="project-box__container">
+                    <p className="project-box__desc">{this.props.projectDescription}</p>
+                    <div className="project-box__badges">
+                        {this.props.badges.map((badge) => <SkillBadge badgeName={badge} />)}
+                    </div>
+                </div>
+
+                <div className="project-box__button-container">
+                    <button>Show Me!</button>
+                    <button>GitHub</button>
+                </div>
             </div>
-        </div>
-
-        <div className="project-box__container">
-            <p className="project-box__desc">{props.projectDescription}</p>
-            <div className="project-box__badges">
-                {props.badges.map((badge) => <SkillBadge badgeName={badge}/>)}
-            </div>
-        </div>
-
-        <div className="project-box__button-container">
-            <button>Show Me!</button>
-            <button>GitHub</button>
-        </div>
-    </div>
-);
-
-export default ProjectBox;
+        );
+    }
+}
