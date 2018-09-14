541055fe5245cb2fe9420a30586f5f3e8e4e5f13
0Calories <cppanand@gmail.com>
2018-09-13 22:03:37 -0400

    Separate project-box styles into another file, add circular icon and strikethrough for Projectbox

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -4,16 +4,19 @@ import SkillBadge from './SkillBadge';
 
 const ProjectBox = (props) => (
     <div className="project-box box">
-        <h1 className="project-box__header box-header">{props.projectName}</h1>
-        <div className="project-box__container">
-            <img 
-                src={"/images/projects/" + props.projectName + ".png"} 
-                className="project-box__image"
-            />
-            <p className="project-box__desc">{props.projectDescription}</p>
-            <div className="project-box__badges">
-                {props.badges.map((badge) => <SkillBadge badgeName={badge}/>)}
+        <div className="project-box__header-container">
+            <h1 className="project-box__header box-header">{props.projectName}</h1>
+            <div className="project-box__header-strike">
+                <img
+                    src={"/images/projects/" + props.projectName + "-icon.png"}
+                    className="project-box__image"
+                />
             </div>
+        </div>
+
+        <p className="project-box__desc">{props.projectDescription}</p>
+        <div className="project-box__badges">
+            {props.badges.map((badge) => <SkillBadge badgeName={badge}/>)}
         </div>
     </div>
 );
