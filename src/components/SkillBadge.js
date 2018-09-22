2e0bc674d806d8a0fa994eed0e9cb977b597dc7c
0Calories <cppanand@gmail.com>
2018-09-21 23:00:17 -0400

    Add conditional animating for SkillBadges

--- src/components/SkillBadge.js
+++ src/components/SkillBadge.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import Bounce from 'react-reveal/Bounce';
 
-const SkillBadge = (props) => (
-    <div>
-        <Bounce right>
-            <div>
-                <a href={props.link} target="_blank">
-                    <img
-                        src={"/images/skills/" + props.badgeName + ".png"}
-                        className="skill-badge"
-                    />
-                </a>
-            </div>
-        </Bounce>
-    </div>
-);
+export default class SkillBadge extends React.Component {
 
-export default SkillBadge;
+    render() {
+        if (this.props.animated) {
+            return (
+                <Bounce right>
+                    <div>
+                        <a href={this.props.link} target="_blank">
+                            <img
+                                src={"/images/skills/" + this.props.badgeName + ".png"}
+                                className="skill-badge"
+                            />
+                        </a>
+                    </div>
+                </Bounce>
+            );
+        } 
+        
+        return (
+            <a href={this.props.link} target="_blank">
+                <img
+                    src={"/images/skills/" + this.props.badgeName + ".png"}
+                    className="skill-badge"
+                />
+            </a>
+        );
+    }
+
+}
--- src/components/SkillsBox.js
+++ src/components/SkillsBox.js
@@ -7,18 +7,18 @@ const SkillsBox = () => (
         <h1 className="skills-box__header box-header">My Skills</h1>
 
         <div className="skills-box__container">
-            <SkillBadge badgeName="Java" link="https://www.oracle.com/java/" />
-            <SkillBadge badgeName="C++" link="https://isocpp.org/" />
-            <SkillBadge badgeName="JavaScript" link="https://www.javascript.com/" />
-            <SkillBadge badgeName="HTML5" link="https://en.wikipedia.org/wiki/HTML5" />
-            <SkillBadge badgeName="CSS3" link="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" />
-            <SkillBadge badgeName="NodeJS" link="https://nodejs.org/en/" />
-            <SkillBadge badgeName="React" link="https://reactjs.org/" />
-            <SkillBadge badgeName="Webpack" link="https://webpack.js.org/" />
-            <SkillBadge badgeName="MongoDB" link="https://www.mongodb.com/" />
-            <SkillBadge badgeName="Mocha" link="https://mochajs.org/" />
-            <SkillBadge badgeName="Bootstrap" link="https://getbootstrap.com/" />
-            <SkillBadge badgeName="jQuery" link="https://jquery.com/" />
+            <SkillBadge badgeName="Java" link="https://www.oracle.com/java/" animated={true} />
+            <SkillBadge badgeName="C++" link="https://isocpp.org/" animated={true} />
+            <SkillBadge badgeName="JavaScript" link="https://www.javascript.com/" animated={true} />
+            <SkillBadge badgeName="HTML5" link="https://en.wikipedia.org/wiki/HTML5" animated={true} />
+            <SkillBadge badgeName="CSS3" link="https://en.wikipedia.org/wiki/Cascading_Style_Sheets" animated={true} />
+            <SkillBadge badgeName="NodeJS" link="https://nodejs.org/en/" animated={true} />
+            <SkillBadge badgeName="React" link="https://reactjs.org/" animated={true} />
+            <SkillBadge badgeName="Webpack" link="https://webpack.js.org/" animated={true} />
+            <SkillBadge badgeName="MongoDB" link="https://www.mongodb.com/" animated={true} />
+            <SkillBadge badgeName="Mocha" link="https://mochajs.org/" animated={true} />
+            <SkillBadge badgeName="Bootstrap" link="https://getbootstrap.com/" animated={true} />
+            <SkillBadge badgeName="jQuery" link="https://jquery.com/" animated={true} />
         </div>
         
     </div>
