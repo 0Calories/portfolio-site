f5554e4eb678f0d0a7c3aaa60b553afcc907ac10
0Calories <cppanand@gmail.com>
2018-09-22 21:14:15 -0400

    Create functionality for sending email upon form submit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,9 +6,15 @@ export default class ContactForm extends React.Component {
     render() {
         return (
             <div className="contact-form-wrapper">
-                <form className="contact-form">
-                    <input className="contact-form__input" type="text" name="email" placeholder="Your Email"/>
-                    <textarea id="contact-form-message" className="contact-form__input" name="message" placeholder="Your Message Here" />
+                <form 
+                    className="contact-form" 
+                    action="https://formspree.io/aanan092@uottawa.ca"
+                    method="POST"
+                >
+                    <input className="contact-form__input" type="email" name="email" placeholder="Your Email" required="required"/>
+                    <input className="contact-form__input" type="text" name="name" placeholder="Your Name" />
+                    <textarea id="contact-form-message" className="contact-form__input" name="message" placeholder="Your Message Here" required />
+                    <button className="contact-form__submit">Send</button> 
                 </form>
             </div>
         );
